141b22e6358b0c86554d16660871f32c19ab8072
Brujitari <natimontesg2@mail.com>
2022-09-27 21:17:27 +0200

    feat: register and login

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { useUser } from 'hooks';
+import Context, { useGetter } from "store";
+
+import App from "./App";
+import Register from "pages/auth/register";
+import Login from "pages/auth/login";
+// import HomePage from './pages/HomePage';
+// import ProductPage from "./pages/ProductPage";
+// import { ProductDetails } from "components/productDetails/ProductDetails";
+// import RouteNotFound from "components/routeNotFound";
+
+// const Guard = ({ component: Component }) => {
+//   const { user } = useGetter();
+//   return user ? Component : <Navigate to="/login" replace />;
+// };
+
+
+const Container = () => {
+  const [user, setUser] = useUser();
+  // const [categories, setCategories] = useCategories();
+  // const [products, setProducts] = useProduct();
+  // const [isCartOpen, setCartOpen] = useState(false);
+  // const [cart, setCart] = useState([]);
+  // const createOrder = useCreateOrder();
+
+
+
+
+  return (
+    <Context.Provider
+      value={{ user, setUser }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} >
+          </Route>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </Context.Provider>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Container />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
